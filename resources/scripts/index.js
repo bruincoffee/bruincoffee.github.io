47ff997d8072bf9ba461d4680f6fb9d01cb7278c
bruincoffee <75443487+bruincoffee@users.noreply.github.com>
2023-06-04 11:49:30 -0700

    ch ch ch changes

---
+++ resources/scripts/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+function clickPic () {
+    let element = document.getElementById("img")
+    
+}
+
+
+
+
+// function toggleShow () {
+//     let element = document.getElementById("img");
+//     element.toggle("overlay");
+// }
